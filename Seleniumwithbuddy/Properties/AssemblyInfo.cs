193a344ac5dc8728bb74ac32504fceac1f9a8583
hadeer-hany <hadeer.hany@moneyfellows.com>
2021-06-14 12:35:19 +0200

    test commit

---
+++ Seleniumwithbuddy/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Seleniumwithbuddy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Seleniumwithbuddy")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("52f0f2e6-d3d5-42c1-91a0-871eceff750e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Seleniumwithbuddy/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.Extensions;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using WDSE;
+using WDSE.Decorators;
+using WDSE.ScreenshotMaker;
+
+namespace Seleniumwithbuddy
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Creating Chrome driver instance:
+            IWebDriver driver = new ChromeDriver(Path.GetFullPath(@"C:\chromedriver_win32"));
+            driver.Navigate().GoToUrl("https://stackoverflow.com/");
+
+
+            // Use the VerticalCombineDecorator to capture entire page:
+            var vcs = new VerticalCombineDecorator(new ScreenshotMaker());
+            var screen = driver.TakeScreenshot(vcs);
+
+
+            //Coverting a byte array to an image:
+            using (Image image = Image.FromStream(new MemoryStream(screen)))
+            {
+                var a = image.Size;
+                image.Save("SeleniumScreenShot.png", ImageFormat.Jpeg);  // Or Png
+            }
+
+
+            //Close Chrome instance:
+            driver.Close();
+        }
+    }
+}
